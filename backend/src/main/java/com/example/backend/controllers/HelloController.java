ea0f4df3ae53ddfd36ce6656d1112116ccfaf100
aguramirez <agustinramirez58@gmail.com>
2025-04-04 18:13:20 -0300

    healthcheck

---
+++ backend/src/main/java/com/example/backend/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Backend online";
+    }
+}
