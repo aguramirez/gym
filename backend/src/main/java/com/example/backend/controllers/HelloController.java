a00918370797fbd57b45e18652351b7d6bb8c914
aguramirez <agustinramirez58@gmail.com>
2025-04-04 21:20:04 -0300

    healthcheck porfa

--- backend/src/main/java/com/example/backend/controllers/HelloController.java
+++ backend/src/main/java/com/example/backend/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.backend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
     }
 }
+
