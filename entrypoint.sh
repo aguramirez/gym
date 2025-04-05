6d0ca1cc4ef27a980e523cdcbd02db01d4d15905
aguramirez <agustinramirez58@gmail.com>
2025-04-04 21:31:53 -0300

    healthcheck porfa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting Spring Boot application with profile: prod"
-
+sleep 5
 # Ejecutar Spring Boot y pasar el puerto que Railway espera
 exec java -jar /app/app.jar --spring.profiles.active=prod --server.port=$PORT
